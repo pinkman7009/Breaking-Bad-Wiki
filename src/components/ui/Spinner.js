7bf2e8cb36833b9295cb9e2010b7c514a8d544e0
Soumik Chaudhuri <soumik.chaudhuri2000@gmail.com>
2020-07-30 19:54:12 +0530

    added spinner

---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import spinner from '../../img/spinner.gif';
+
+const Spinner = () => {
+	return <img src={spinner} className="spinner" alt="spinner" />;
+};
+
+export default Spinner;
