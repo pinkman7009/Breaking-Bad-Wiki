2a9aa1e408391410ab39f87398926a1da3e16328
Soumik Chaudhuri <soumik.chaudhuri2000@gmail.com>
2020-07-30 16:33:11 +0530

    cards added

---
+++ src/components/ui/Search.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Search = ({ getQuery }) => {
+	const [ text, setText ] = useState('');
+
+	const onChange = (query) => {
+		setText(query);
+		getQuery(query.toLowerCase());
+	};
+	return (
+		<section className="search">
+			<form>
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Search characters"
+					value={text}
+					onChange={(e) => onChange(e.target.value)}
+					autoFocus
+				/>
+			</form>
+		</section>
+	);
+};
+
+export default Search;
