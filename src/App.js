e7fa2cc55e6d97636c5820641383f7ea0230c44e
Soumik Chaudhuri <soumik.chaudhuri2000@gmail.com>
2020-07-30 14:39:30 +0530

    npm fixes done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/ui/Header';
 import './App.css';
 
 const App = () => {
+	const [ items, setItems ] = useState([]);
+	const [ isLoading, setIsLoading ] = useState(true);
+
+	useEffect(() => {});
+
 	return (
 		<div className="container">
 			<Header />
